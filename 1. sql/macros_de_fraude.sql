e931039475a29ce45418e8793eb2a29090e0f3f1
Gustavo Barros <contato.gustavo.lima.brros@gmail.com>
2026-06-02 14:07:55 -0300

    Phase 1: Add folders: queries and results for better management pt.2

--- 1. sql/macros_de_fraude.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-    COUNT(*) AS total_fraude,
-	SUM(amount) AS prejuizo_total,
-	MAX(amount) AS maior_transacao_fraudulenta,
-    ROUND(AVG(amount), 2) AS valor_fraude_media_total
-FROM public.deteccao_fraude
-WHERE class = 1 AND amount > 0.0;
--- 1. sql/media_ticket_fraudulento_por_hora.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-    FLOOR(time / 3600) AS hora_do_dia,
-    SUM(class) AS total_fraudes,
-	ROUND(AVG(amount), 2) AS media_valor_fraude
-FROM public.deteccao_fraude
-WHERE class = 1 and amount > 0.0
-GROUP BY FLOOR(time / 3600)
-ORDER BY total_fraudes DESC;
